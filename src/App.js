e204e02d7fd79e7830a5a6e3c3ede0b95ba060eb
Praneshxo <praneshgara@gmail.com>
2024-12-10 09:59:48 +0530

    raydot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Home from './pages/Home';
 import './App.css';
+import Nlp from './pages/Nlp';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <div className="main-content">
+          <Header />
+          <div className='page-content'>
+          <Routes>
+            <Route path="/" element={<Home />} /> 
+            <Route path="/nlp" element={<Nlp />} />
+          </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Chatbot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chatbot.css';
+function Chatbot() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Chatbot
